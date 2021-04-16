818efd337badb3e513bd3d99fbba38644e357e74
Sergey Grozov <grozoff92@gmail.com>
2021-04-16 23:14:08 +0500

    Добавлен пользовательский ввод
    Отлажен парсер команд
    Добавлены основные команды

---
+++ File Manager/FileManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace File_Manager
+{
+    public class FileManager
+    {
+        public void Information(string path)
+        {
+            FileInfo fileInf = new FileInfo(path);
+            Console.WriteLine(string.Format("Information about {0} :\nSize = {1} bytes\nLast modified on {2}\nCreation time on {3}", fileInf.Name, fileInf.Length, fileInf.LastWriteTime, fileInf.CreationTime));
+        }
+
+        public void Remove(string path)
+        {
+            // TODO: упростить!!!!
+            FileInfo fileInf = new FileInfo(path);
+            DirectoryInfo dirInf = new DirectoryInfo(path);
+            if (Directory.Exists(path))
+            {
+                if (dirInf.Exists)
+                {
+                    dirInf.Delete(true);
+                    Console.WriteLine("Папка удалена.");
+                }
+                else
+                {
+                    Console.WriteLine("Папка не найдена.");
+                }
+            }
+            else if (File.Exists(path))
+            {
+                if (fileInf.Exists)
+                {
+                    fileInf.Delete();
+                    Console.WriteLine("Файл удален.");
+                }
+                else
+                {
+                    Console.WriteLine("Файл не найден.");
+                }
+            }
+        }
+
+        public void MkFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName).Close();
+                Console.WriteLine("Файл создан");
+            }
+            else
+            {
+                Console.WriteLine("Файл с таким именем уже существует");
+            }
+        }
+
+        public void MkDir(string dirName)
+        {
+            try
+            {
+                if (!Directory.Exists(dirName))
+                {
+                    Directory.CreateDirectory(dirName);
+                    Console.WriteLine("Папка создана");
+                }
+                else
+                {
+                    Console.WriteLine("Папка с таким именем уже существует");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Невозможно создать папку с таким именем");
+            }
+
+        }
+
+        public void Copy(string path, string newPath)
+        {
+            bool overwrite = false;
+            FileInfo fileInf = new FileInfo(path);
+
+            if (File.Exists(path))
+            {
+                while (true)
+                {
+                    try
+                    {                       
+                        if (fileInf.Exists && overwrite == true)
+                        {
+                            File.Copy(path, newPath, overwrite);
+                            Console.WriteLine("Файл скопирован с заменой");
+                            break;
+                        }
+                        else if (fileInf.Exists && overwrite == false)
+                        {
+                            File.Copy(path, newPath, overwrite);
+                            Console.WriteLine("Файл скопирован");
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write("Файл не найден.");
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        Console.Write("Файл с таким именем уже существует в заданной директории. Заменить? Y/N :  "); 
+                        // TODO: переделать момент с заменой файлов
+                        string userAnswer = Console.ReadLine().ToUpper();
+                        if (userAnswer == "Y")
+                        {
+                            overwrite = true;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (Directory.Exists(path))
+            {
+                DirectoryInfo dirInf = new DirectoryInfo(path);
+                DirectoryInfo[] dirs = dirInf.GetDirectories();
+
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+
+                FileInfo[] files = dirInf.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    string temppath = Path.Combine(newPath, file.Name);
+                    file.CopyTo(temppath, true);
+                }
+
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(newPath, subdir.Name);
+                    Copy(subdir.FullName, temppath);
+                }
+                Console.WriteLine("Папка с файлами скопирована"); // TODO: Исправить вывод сообщения (сейчас выходит много раз)
+            }
+            else
+            {
+                Console.WriteLine("Нужно указать пути файла или директории");
+            }
+        }
+    }
+}
--- File Manager/Program.cs
+++ File Manager/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace File_Manager
 {
@@ -6,7 +9,93 @@ namespace File_Manager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "File Manager";
+            FileManager fm = new FileManager();
+
+            Console.WriteLine("[help]   [cd]    [ls]    [inf]   [rm]    [mkfil]    [mkdir]    [cp]    [exit]\n");
+
+            string currentDirectory = Directory.GetCurrentDirectory(); // TODO: сохранять состояние текущей директории
+            string userCommand = string.Empty;
+            List<string> parameters = new List<string>();
+
+            #region Заглушки
+            string file = @"C:\Users\magnu\source\repos\123.txt";
+            string newFile = @"C:\Users\magnu\source\repos\12\123.txt";
+            string pathDir = @"C:\Users\magnu\Desktop\123";
+            string newPathDir = @"C:\Users\magnu\source\repos\12";
+            #endregion
+
+            bool exit = true;
+
+            while (exit)
+            {
+                Console.WriteLine(string.Format(currentDirectory));
+
+                
+                Console.Write("Command: ");
+                userCommand = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(userCommand))
+                {
+                    continue;
+                }
+
+                string[] parts1 = userCommand.Split(new string[] { " \"" }, StringSplitOptions.None);
+                string[] clearCommands = new string[4];
+
+                for (int i = 0; i < parts1.Length; i++)
+                {
+                    clearCommands[i] = parts1[i].Trim();
+                    clearCommands[i] = Regex.Replace(clearCommands[i], "\"", "");                    
+                }
+                
+
+                //string[] parts2 = Regex.Split(userCommand, " \"");
+
+                // TODO: парсинг путей с пробелами, пока выход сделать сплит не пробелом а запрещенным символом
+
+                //parameters.RemoveAll(WhiteParameters);
+
+                userCommand = clearCommands[0].ToLower();
+
+                // TODO: cd для навигации, ls для пэйджинга, inf для информации о файле/папки
+                // TODO: Ловля исключений
+                // TODO: Отловить исключение с заменой файлов через foreach
+
+
+                switch (userCommand)
+                {
+                    case "inf":
+                        fm.Information(clearCommands[1]);
+                        break;
+                    case "rm":
+                        fm.Remove(clearCommands[1]);
+                        break;
+                    case "mkfil":
+                        fm.MkFile(clearCommands[1]);
+                        break;
+                    case "mkdir":
+                        fm.MkDir(clearCommands[1]);
+                        break;
+                    case "cp":
+                        fm.Copy(file, newFile);
+                        break;
+                    case "exit":
+                        exit = false;
+                        Console.Clear();
+                        break;
+                    case "help":
+                        Console.Clear();
+                        Console.WriteLine("[help] - Вызвать справку\n[cd] - навигация по каталогам\ncd .. переход в родительский каталог\n" +
+                            "cd \\путь к каталогу на текущем диске, например cd \\users[rm] - Удаление файла\\каталога\n" +
+                            "[mkfil] - Создание файла\n[mkdir] - Создание каталога\n[cp] - Копирование файла\\директории\n" +
+                            "[exit] - Завершить работу файлового менеджера\n");
+                        break;
+                    default:
+                        Console.WriteLine("Команда не найдена");
+                        break;
+                }
+            }           
         }
     }
 }
